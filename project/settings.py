12ccf9bc24aa52daa8492c1f372239084af22940
Abhayrcb <abhaychoudhary4578@gmail.com>
2026-01-15 19:22:38 +0530

    mysql setup

--- project/settings.py
+++ project/settings.py
@@ -86,13 +86,23 @@ AUTH_USER_MODEL ='accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ExperimentWithCo$default',
+        'USER':'ExperimentWithCo',
+        'PASSWORD':'',
+        'HOST':'ExperimentWithCode.mysql.pythonanywhere-services.com',
+        'PORT':'3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
